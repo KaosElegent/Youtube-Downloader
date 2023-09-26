844b1d981b4f334f5170fe8a14248b1588f5107d
Shrey Ajaykumar Bhatt <sabhatt2@myseneca.ca>
2023-09-25 23:10:49 -0400

    Basic 1 video downlading functionality added.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("HI")
+from pytube import YouTube
+
+def Download(link):
+    try:
+        video = YouTube(link)
+    except:
+        print("Invalid URL. Try Again.")
+        return -1
+    
+    video = video.streams.get_highest_resolution()
+    try:
+        video.download()
+        print("Download is completed successfully")
+    except:
+        print("An error has occurred")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
